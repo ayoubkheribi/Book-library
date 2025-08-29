db70930bf672d9dd2f4ced701991229d55583cb8
Ayoub <akhribi627@gmail.com>
2025-08-29 20:18:53 +0100

    add logo

--- book-reco/src/components/NavBar.jsx
+++ book-reco/src/components/NavBar.jsx
@@ -10,7 +10,9 @@ const  Navbar = ({menuOpen, setMenuOpen}) => {
     <nav className="fixed top-0 w-full z-40 bg-transparent backdrop-blur-lg border-b border-white/10 shadow-lg">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
-          <Link to="/" className="text-2xl text-black font-bold hover:text-[#753e0e]">Book <span className='text-[#753e0e] hover:text-black'>.Reco</span></Link>
+          <Link to="/" className="text-2xl text-black font-bold hover:text-[#753e0e]">
+          <img src={logo} alt="Book.Reco Logo" className="h-10 w-10" />
+          Book <span className='text-[#753e0e] hover:text-black'>.Reco</span></Link>
           <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" onClick={() => setMenuOpen((prev) => !prev)}>
             &#9776;
           </div>
